39362faa26b4587c9da030bbd1eef66db3d3d868
Iree Hale <KidFlash2099@protonmail.com>
2024-10-29 02:17:52 -0400

    making change from css to scss and will begin working on navbar and hero section

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -1,34 +1,19 @@
-import './App.css'
+import './App.scss'
+import './reset.scss'
 
 function App() {
 
   return (
     <body>
-        <div className="navbar-section">
-            <nav class="navbar navbar-expand-lg navbar-light bg-dark style="color: white">
-              <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar w/ text</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                  </ul>
-                  <span class="navbar-text">
-                    Navbar text with an inline element
-                  </span>
-                </div>
-              </div>
-            </nav>
+        <div className="navbar">
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
         </div>
       <div className="hero-section"></div>
     </body>
