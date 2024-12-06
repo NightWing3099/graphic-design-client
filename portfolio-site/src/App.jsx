8f382b142a9f25202e88f76dfee05ea17f01e204
Iree Hale <KidFlash2099@protonmail.com>
2024-12-06 07:33:30 -0500

    working on the styles of the about me section will be working on the next of the project

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -20,19 +20,18 @@ function App() {
       </div>
       <div className="about-me">
         <div className="about-info">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id blandit dolor, sit amet consectetur quam. 
-            Quisque blandit nisi a nibh finibus bibendum eu sed massa. Fusce nisi augue, gravida a enim id, lobortis lobortis justo. Nam sit amet dignissim nulla, sed tincidunt risus. 
-            Sed libero urna, ultrices et ex id, iaculis accumsan nulla. Proin non ante eu tellus suscipit imperdiet vel vel nisl. Nam commodo nunc vel libero scelerisque fringilla. 
-            Duis commodo pharetra elementum. Quisque facilisis mauris nisl, nec sodales quam rhoncus id. Aliquam quis metus tincidunt, fringilla diam a, dapibus arcu. </p>
-
-            <p>Nullam sagittis a lacus quis semper. Vestibulum nec fringilla eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Maecenas pretium hendrerit lacus, et interdum lorem interdum non. Sed lobortis ultricies efficitur. Morbi rhoncus lorem sed justo sagittis porttitor. 
-            Mauris dictum lectus nec enim consequat commodo. In aliquet, nibh et pulvinar pharetra, enim nisl maximus dui, vel vestibulum eros eros eu est. 
-            Morbi lacinia est eget aliquam sodales.</p>
+          <p>
+            Hi, I’m a graphic designer with 10 years of experience turning ideas into designs that don’t just look good—they work. 
+            I use sales psychology and copywriting techniques to create visuals that connect with people and inspire action. 
+            Whether it’s building a brand, designing a website, or crafting marketing materials, 
+            I focus on making every project resonate with the audience and deliver results. 
+            Let’s work together to bring your vision to life.
+          </p>
 
         </div>
         <div className="about-img">hi</div>
       </div>
+      <div className="Portfolio-section"></div>
     </body>
   )
 }
