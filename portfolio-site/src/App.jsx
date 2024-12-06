ec95d4d1a66bcc1632cee6aa8faa3e08c79c68ca
Iree Hale <KidFlash2099@protonmail.com>
2024-12-05 20:24:57 -0500

    working on the about section of the project

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -19,7 +19,19 @@ function App() {
           <img className="hero-img" src="../images/pexels-tranmautritam-326501.jpg"  height={600} width={1366}/>
       </div>
       <div className="about-me">
-        About the designer
+        <div className="about-info">
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id blandit dolor, sit amet consectetur quam. 
+            Quisque blandit nisi a nibh finibus bibendum eu sed massa. Fusce nisi augue, gravida a enim id, lobortis lobortis justo. Nam sit amet dignissim nulla, sed tincidunt risus. 
+            Sed libero urna, ultrices et ex id, iaculis accumsan nulla. Proin non ante eu tellus suscipit imperdiet vel vel nisl. Nam commodo nunc vel libero scelerisque fringilla. 
+            Duis commodo pharetra elementum. Quisque facilisis mauris nisl, nec sodales quam rhoncus id. Aliquam quis metus tincidunt, fringilla diam a, dapibus arcu. </p>
+
+            <p>Nullam sagittis a lacus quis semper. Vestibulum nec fringilla eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Maecenas pretium hendrerit lacus, et interdum lorem interdum non. Sed lobortis ultricies efficitur. Morbi rhoncus lorem sed justo sagittis porttitor. 
+            Mauris dictum lectus nec enim consequat commodo. In aliquet, nibh et pulvinar pharetra, enim nisl maximus dui, vel vestibulum eros eros eu est. 
+            Morbi lacinia est eget aliquam sodales.</p>
+
+        </div>
+        <div className="about-img">hi</div>
       </div>
     </body>
   )
